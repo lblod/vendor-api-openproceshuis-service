484b77f0c6852e6f0ca144061f033e10c97dce76
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-12-02 10:46:53 +0100

    feat: check if the session uri is linked to an account
    - move the authRequest to a controller
    - add method to check the auth before doing an action
    
    Refs: oph-833

---
+++ controller/auht.ts
@@ -0,0 +1,54 @@
+import { sparqlEscapeUri, query } from 'mu';
+import { Request } from 'express';
+
+import { HttpError } from '../util/http-error';
+
+export const SESSION_GRAPH_URI =
+  process.env.SESSION_GRAPH || 'http://mu.semte.ch/graphs/sessions';
+
+export async function authenticateBeforeAction(request: Request) {
+  const sessionUri = sessionUriFromRequest(request);
+  const accountUri = await getAccountForSessionUri(sessionUri);
+
+  if (!accountUri) {
+    throw new HttpError(
+      'No account found for session',
+      401,
+      'The used session is not active or linked to a known account.',
+    );
+  }
+}
+
+function sessionUriFromRequest(request: Request) {
+  const HEADER_MU_SESSION_ID = 'mu-session-id';
+  const sessionUri = request.get(HEADER_MU_SESSION_ID);
+
+  if (!sessionUri) {
+    throw new HttpError(
+      'Missing session header',
+      400,
+      `The session header '${HEADER_MU_SESSION_ID}' was not found in the request headers.`,
+    );
+  }
+
+  return sessionUri;
+}
+
+async function getAccountForSessionUri(sessionUri: string) {
+  if (!sessionUri) {
+    return null;
+  }
+
+  const sparqlResult = await query(`
+    PREFIX session: <http://mu.semte.ch/vocabularies/session/>
+
+    SELECT ?account
+    WHERE {
+      GRAPH ${sparqlEscapeUri(SESSION_GRAPH_URI)} {
+          ${sparqlEscapeUri(sessionUri)} session:account ?account .
+      }
+    } LIMIT 1
+  `);
+
+  return sparqlResult.results.bindings?.[0]?.account.value;
+}
--- router/process.ts
+++ router/process.ts
@@ -1,14 +1,16 @@
 import Router from 'express-promise-router';
 
 import { Request, Response } from 'express';
-import { AuthRequest } from '../request/auth';
+
 import { HttpError } from '../util/http-error';
+import { authenticateBeforeAction } from '../controller/auht';
 
 export const processRouter = Router();
 
 processRouter.post('/', async (req: Request, res: Response) => {
   try {
-    AuthRequest.fromRequest(req);
+    await authenticateBeforeAction(req);
+
     return res.status(201).send();
   } catch (error) {
     const errorResponse = HttpError.caughtErrorJsonResponse(error);
